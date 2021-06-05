4692ab9d99c1b5446776b50e08d545584572c754
Carol Casagrande <ca_casag@yahoo.com.br>
2021-06-05 12:10:25 -0300

    ajuste layout page login

--- src/views/pages/Login/index.tsx
+++ src/views/pages/Login/index.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from '../../../img/logo.png';
 import login from '../../../img/atend.jpg';
-import { Container } from '../Login/style'
-import { FormContent } from '../Login/style'
 import { FiArrowRight, FiChevronRight } from 'react-icons/fi';
-import '../Login/style'
+import './style.css'
 
 const Login: React.FC = () =>{
   return (
-    <div className="App">
+    <div className="login">
       <body>
-      <Container>
-        <header className="App-header">        
-          <a className="App-link" href="/" target="_blank">
-              <img src={logo} className="App-logo" alt="logo" />
+      <div className="container">
+        <header className="header">        
+          <a className="home-link" href="/" target="_blank">
+              <img src={logo} className="logo" alt="logo" />
           </a>              
         </header>     
         <main>   
         <aside className="aside">
           <img src={login} className="img-login" alt="login" />
         </aside>
-        <FormContent>
+        <div className="form-content">
           <form /* onSubmit={handleLogin} */ className="form-login  ">
             <h1>Faça seu Login</h1>
             <input type="text" id="name" placeholder="Digite o seu nome completo" /* value={login} onChange={(e) => setLogin(e.target.value)} required */ />
@@ -28,12 +26,12 @@ const Login: React.FC = () =>{
             <input type="password" id="senha" placeholder="Digite sua senha" /* value={password} onChange={(e) => setPassword(e.target.value)} required  *//>
             <button type="submit">Acessar <FiArrowRight size={25} /></button>
           </form>
-        </FormContent>
+        </div>
         </main>
         <footer className="footer">
           <p> Curso Gama&Afya - Desenvolvido por G2T2 | Ariele, Caroline e Fernando </p>
         </footer>
-        </Container>
+        </div>
       </body>
     </div>
   );
