b686018f50294716920041db91b6fd2a52d0616e
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-06-10 23:46:01 -0300

    fix bug -  changed FormEvent type to React.FormEvent

--- src/views/pages/Login/index.tsx
+++ src/views/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {  useState, useEffect } from 'react';
 import login from '../../../img/atend.jpg';
 import { FiArrowRight, FiChevronRight } from 'react-icons/fi';
 import './style.css'
@@ -9,7 +9,7 @@ const Login: React.FC = () => {
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
 
-  function handleLogin(event:FormEvent<HTMLFormElement>) {
+  function handleLogin(event: React.FormEvent<HTMLFormElement>) {
     event?.preventDefault()
 
     const user = {
@@ -36,64 +36,23 @@ const Login: React.FC = () => {
   return (
     <div className="login">
       <body>
-      <div className="container">    
-        <main>   
-        <aside className="aside-left">
-          <img src={login} className="img-login" alt="login" />
-        </aside>
-        <aside className="aside-right">
-          <div className="form-content">
-            <form  onSubmit={handleLogin} className="form-login  ">
-              <h1>Faça seu Login</h1>
-              <input type="text" id="login" placeholder="Digite o seu login" value={login} onChange={(e) => setLogin(e.target.value)} required />
-              <input type="password" id="senha" placeholder="Digite sua senha"  value={password} onChange={(e) => setPassword(e.target.value)} required  />
-              <button type="submit">Acessar <FiArrowRight size={25} /></button>
-            </form>
+        <div className="container">    
+          <main>   
+          <aside className="aside-left">
+            <img src={login} className="img-login" alt="login" />
+          </aside>
+          <aside className="aside-right">
+            <div className="form-content">
+              <form  onSubmit={handleLogin} className="form-login  ">
+                <h1>Faça seu Login</h1>
+                <input type="text" id="login" placeholder="Digite o seu login" value={login} onChange={(e) => setLogin(e.target.value)} required />
+                <input type="password" id="senha" placeholder="Digite sua senha"  value={password} onChange={(e) => setPassword(e.target.value)} required  />
+                <button type="submit">Acessar <FiArrowRight size={25} /></button>
+              </form>
 
-          </div>
-        </aside>
-        </main>
-        <footer className="footer">
-          <p>Curso Gama & Afya - Desenvolvido por G2T2</p>
-          <section className="redesocial">
-            <div className="contato">
-              <p>Andre</p>
-              <a target="_blank" href="https://github.com/andreludwig">
-                <img src={github} alt="logo github" />
-              </a>
-              <a target="_blank" href="https://www.linkedin.com/in/andr%C3%A9-ludwig-67b17414a/">
-                <img src={linkedin} alt="logo linkedin" />
-              </a>        
             </div>
-            <div className="contato">
-              <p>Ariele</p>
-              <a target="_blank" href="https://github.com/ArieleM">
-                <img src={github} alt="logo github" />
-              </a>
-              <a target="_blank" href="https://www.linkedin.com/in/pereiramendonca/">
-                <img src={linkedin} alt="logo linkedin" />
-              </a>              
-            </div>
-            <div className="contato">
-              <p>Caroline</p>
-             <a target="_blank" href="https://github.com/carolcasagrande">
-                <img src={github} alt="logo github" />
-              </a>
-              <a target="_blank" href="https://www.linkedin.com/in/carolinecasagrande">
-                <img src={linkedin} alt="logo linkedin" />
-              </a>              
-            </div>
-            <div className="contato">
-              <p>Fernando</p>
-              <a target="_blank" href="https://github.com/ofernandovegano">
-                <img src={github} alt="logo github" />
-              </a>
-              <a target="_blank" href="https://www.linkedin.com/in/fernandogomesrodrigues/">
-                <img src={linkedin} alt="logo linkedin" />
-              </a>      
-            </div>
-            </section> 
-        </footer>
+          </aside>
+          </main>
         </div>
       </body>
     </div>
