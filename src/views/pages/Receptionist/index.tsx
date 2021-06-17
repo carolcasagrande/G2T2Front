19b6ff542f0b7b94c394c76495d1e06e184b05d2
Ariele Mendonça <ariele_apm@outlook.com>
2021-06-17 11:53:31 -0300

    Update accordion

--- src/views/pages/Receptionist/index.tsx
+++ src/views/pages/Receptionist/index.tsx
@@ -22,10 +22,8 @@ import './styles.css';
 
 const Receptionist: React.FC = () => {
   const [checkins, setCheckins] = useState<Array<any>>([])
-  const [queue, setQueue] =  useState<Boolean>(true)
   const [pageActive, setPageActive] = useState('');
-
-  const [expanded, setExpanded] = React.useState<string | false>(false);
+  const [expanded, setExpanded] = React.useState<string | false>('panel1');
 
   const handleChange = (panel: string) => (event: React.ChangeEvent<{}>, isExpanded: boolean) => {
     setExpanded(isExpanded ? panel : false);
@@ -55,17 +53,6 @@ const Receptionist: React.FC = () => {
       channel.unsubscribe();
     }
   }, [checkins])
-
-  console.log(checkins);
-  const handleState = (page:string) =>{
-    // setPageActive()
-    console.log(pageActive);
-    
-  }
-
-  const handleClick = () => {
-    setQueue(!queue)
-  }
   
   return(
     <div className="nav-cards">
@@ -95,22 +82,27 @@ const Receptionist: React.FC = () => {
                     id="panel1bh-header"
                     className="accordion-receptionist"
                   >
-                    <div className="header-receptionist">
+                    <div className="header-receptionist receptionist-none-border">
                       <HomeHeader  title='Agendamentos do dia' />
                     </div>
                   </AccordionSummary>
-                  <AccordionDetails >
+
                     <div className="header-receptionist">
+
                       <PatientsHead />
+
                       <div className='column-body'>
                         {
-                        checkins.map( (checkin: any) => {
-                            return <PatientsBody key={checkin._id} checkin={ checkin } />
-                        })
+                          checkins.length > 1 ?
+                            checkins.map( (checkin: any) => {
+                                return <PatientsBody key={checkin._id} checkin={ checkin } />
+                            })
+                          :
+                          <p>Sem pacientes na fila de espera.</p>
                         }
                       </div>
                     </div>
-                  </AccordionDetails>
+
                 </Accordion>
                 <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
                   <AccordionSummary
@@ -118,19 +110,22 @@ const Receptionist: React.FC = () => {
                     aria-controls="panel2bh-content"
                     id="panel2bh-header"
                   >
-                    <div className="header-receptionist">
+                    <div className="header-receptionist receptionist-none-border">
                       <HomeHeader title='Checkins' />
                     </div>
                   </AccordionSummary>
-                  <AccordionDetails>
                     <div className='column-body header-receptionist'>
-                        {
-                        checkins.map( (checkin: any) => {
-                            return <PatientsBody key={checkin._id} checkin={ checkin } />
-                        })
-                        }
-                      </div>
-                  </AccordionDetails>
+                      <PatientsHead />
+
+                      {
+                        checkins.length > 1 ?
+                          checkins.map( (checkin: any) => {
+                              return <PatientsBody key={checkin._id} checkin={ checkin } />
+                          })
+                        :
+                        <p>Nenhum paciente agendado para hoje.</p>
+                      }
+                    </div>
                 </Accordion>
             </div>
         </div>        
@@ -148,7 +143,6 @@ const Receptionist: React.FC = () => {
             </>
           }
         </div>
-
       </div>
     </div>
     </div>
