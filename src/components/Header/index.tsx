cab4d902c0c21c6b759123afc66a343ae6304905
Carol Casagrande <ca_casag@yahoo.com.br>
2021-06-17 23:18:30 -0300

    ajuste login e logo header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import ImgLogin from '../../img/logo-maisha.png'
 import './styles.css'
 
 const Header: React.FC = () => (
   <header className="header">  
     <div className="header-logo">      
       <a className="home-link" href="/">
-        <div className='logo'>Maisha</div>
+        <div className='logo'>
+          <img src={ImgLogin} alt="logo" />
+        </div>
       </a>   
     </div>          
   </header>  
