1a0136c903c7d01fe4b9c75139ecf71a1259e7a8
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-06-18 20:57:19 -0300

    deleted routes we were not using anymore

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,22 +4,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './views/pages/Login';
 import Specialist from './views/pages/Specialist';
 import Receptionist from './views/pages/Receptionist';
-import Occupation from './views/pages/Occupation';
-import Record from './views/pages/Record'; 
-import MedicalRecordForm from './components/Medical-record-form';
- 
+
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Specialist} />
+        <Route path="/" exact component={Login} />
         <Route path="/receptionist" component={Receptionist} />
-        <Route path="/login" component={Login} />
         <Route path="/specialist/appointment" component={Specialist} />
         <Route path="/specialist/history" component={Specialist} />
-        <Route path="/occupation" component={Occupation} />
-        <Route path="/record" component={Record} />
-        <Route path="/history" component={MedicalRecordForm} />
+
       </Switch>
     </BrowserRouter>
   );
