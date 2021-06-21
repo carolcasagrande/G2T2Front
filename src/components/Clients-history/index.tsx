b53453b34533463c1eee24a6c0ac4a4f9141be9e
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-06-21 13:16:16 -0300

    search feature implemented

--- src/components/Clients-history/index.tsx
+++ src/components/Clients-history/index.tsx
@@ -7,17 +7,24 @@ import HomeHeader from '../Home-header';
 import SearchInput from '../Search-input';
 import Client from '../Client';
 
+import SearchIcon from '@material-ui/icons/Search';
+
 const ClientsHistory: React.FC = () => {
   const services = useSelector((state: any) => state.services.servicesDone)
-
+  const [searchField, setSearchField] = useState<String>('');
+  
+  const filteredServices = services?.filter((service: any ) =>
+    service.client.name.toLowerCase().includes(searchField.toLowerCase())
+  )
+  
   return(
     <div className="medical-records-history-container">
-      <HomeHeader title='Pacientes' />
-      <SearchInput />
+      <HomeHeader title='Pacientes' />  
+      <SearchInput setSearchField={setSearchField}/>
       {
         services ?
           (
-            services.map( (service: any) => {
+            filteredServices.map( (service: any) => {
             return (
                 <Client service={service} key={service.id}/>
             )
--- src/components/Search-input/index.tsx
+++ src/components/Search-input/index.tsx
@@ -4,14 +4,14 @@ import SearchIcon from '@material-ui/icons/Search';
 
 import './styles.css';
 
-const SearchInput: React.FC = () =>(
+const SearchInput: React.FC = ({setSearchField}) =>(
 
-    <div className="search">
-      <div className="search-input">
-        <SearchIcon style={{color: '#13132B'}}/>
-        <input placeholder="Buscar pacientes" />
-      </div>
-    </div>
+  <div className="search">
+  <div className="search-input">
+    <SearchIcon style={{color: '#13132B'}}/>
+    <input onChange={((e) => setSearchField(e.target.value))} placeholder="Buscar pacientes" />
+  </div>
+  </div>
 
 );
 
